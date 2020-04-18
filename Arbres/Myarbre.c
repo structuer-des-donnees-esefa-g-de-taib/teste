a33945eb24127ecedca453f3e8c4329dfd4124a8
taib1999 <56398779+taib1999@users.noreply.github.com>
2020-04-18 02:32:21 +0200

    Add files via upload

---
+++ Arbres/Myarbre.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+typedef struct arbre Arbre;
+typedef struct list List;
+
+struct arbre
+{
+    int val;
+    List *enfants;
+};
+
+struct list
+{
+    Arbre *node;
+    List *next;
+};
+
+List *cons(Arbre *arbre, List *liste)
+{
+    List *elem;
+    if ((elem = malloc(sizeof *elem)) == NULL)
+        return NULL;
+    elem->node = arbre;
+    elem->next = liste;
+    return elem;
+}
+
+int main(void)
+{
+    Arbre G = {'G', NULL}, H = {'H', NULL}, I = {'I', NULL};
+    Arbre F = {'F', cons(&G, cons(&H, cons(&I, NULL)))};
+    Arbre D = {'D', NULL}, E = {'E', NULL};
+    Arbre C = {'C', cons(&F, NULL)};
+    Arbre B = {'B', cons(&D, cons(&E, NULL))};
+    Arbre A = {'A', cons(&B, cons(&C, NULL))};
+    return 0;
+}
+
